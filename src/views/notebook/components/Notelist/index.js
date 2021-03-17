0b2cc7a8b2a4d2be7d762a5938aca53aa3762a78
aiteam_chentao <chentao9602@163.com>
2021-03-17 20:23:06 +0800

    init notelist

--- src/views/notebook/components/Notelist/index.js
+++ src/views/notebook/components/Notelist/index.js
@@ -1,15 +1,60 @@
 import React from 'react'
 import {
-  NotelistWrapper
+  NotelistWrapper,
+  UpCreateBtn,
+  Notes,
 } from './style'
 
 class Notelist extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      noteList: [],
+    }
+  }
+
   render(){
+    const { noteList } = this.state
     return (
       <NotelistWrapper>
-        notelist
+        <UpCreateBtn>+ new</UpCreateBtn>
+        <Notes>
+          {
+            noteList.map(item => {
+              return (
+                <li key={item.id}>
+                  <div className="title">{item.title}</div>
+                  <div className="update-time">{item.update_time}</div>
+                </li>
+              )
+            })
+          }
+        </Notes>
       </NotelistWrapper>
     )
+  }
+
+  componentDidMount(){
+    const noteList = [
+      {
+        id: 1,
+        title: 'note1',
+        update_time: '2020/08/01',
+      },
+      {
+        id: 2,
+        title: 'note2',
+        update_time: '2020/08/02',
+      },
+      {
+        id: 3,
+        title: 'note3',
+        update_time: '2020/08/03',
+      },
+    ]
+    this.setState({
+      noteList,
+    })
   }
 }
 
--- src/views/notebook/components/Notelist/style.js
+++ src/views/notebook/components/Notelist/style.js
@@ -1,8 +1,39 @@
 import styled from 'styled-components'
 
+const borderColor = '#eee'
+
 export const NotelistWrapper = styled.div`
   width: 300px;
   height: 100%;
   overflow-y: scroll;
   background: #fff;
+`
+
+export const UpCreateBtn = styled.div`
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  padding: 0 20px;
+  border-bottom: 1px solid ${borderColor};
+  cursor: pointer;
+`
+
+export const Notes = styled.ul`
+  width: 100%;
+
+  li{
+    height: 50px;
+    border-bottom: 1px solid ${borderColor};
+    padding: 5px 20px;
+
+    .title{
+      line-height: 30px;
+      font-weight: 500;
+    }
+
+    .update-time{
+      line-height: 20px;
+      font-size: 12px;
+    }
+  }
 `
