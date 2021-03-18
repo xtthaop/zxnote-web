8abb77b165ec496fcf2035e69b6a92218b3017c2
aiteam_chentao <chentao9602@163.com>
2021-03-19 00:31:05 +0800

    init toolbar

--- src/views/notebook/components/Editor/index.js
+++ src/views/notebook/components/Editor/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import SvgIcon from '@/components/SvgIcon'
 import {
-  EditorWrapper
+  EditorWrapper,
+  TitleWrapper,
+  ToolBar,
 } from './style'
 
 class Editor extends React.Component {
   render(){
     return (
       <EditorWrapper>
-        editor
+        <TitleWrapper>
+          <div className="save-status">saved</div>
+          <input className="custom-input" defaultValue="Title"></input>
+        </TitleWrapper>
+        <ToolBar>
+          <li className="tool right">
+            <SvgIcon iconClass="release"></SvgIcon>
+            <span className="release">发布笔记</span>
+          </li>
+          <li className="tool"><SvgIcon iconClass="pic"></SvgIcon></li>
+          <li className="tool right"><SvgIcon iconClass="save"></SvgIcon></li>
+        </ToolBar>
       </EditorWrapper>
     )
   }
--- src/views/notebook/components/Editor/style.js
+++ src/views/notebook/components/Editor/style.js
@@ -9,3 +9,63 @@ export const EditorWrapper = styled.div`
   height: 100%;
   background: #fffff9;
 `
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  height: 70px;
+
+  .save-status{
+    height: 20px;
+    line-height: 20px;
+    font-size: 12px;
+    padding-right: 5px;
+    color: #ccc;
+    text-align: right;
+  }
+
+  .custom-input{
+    width: 100%;
+    height: calc(100% - 20px);
+    border: none;
+    background: transparent;
+    padding: 0 30px 15px;
+    font-size: 30px;
+    outline: none;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
+
+export const ToolBar = styled.ul`
+  width: 100%;
+  background: #ccc;
+  padding: 0 10px;
+  box-sizing: border-box;
+
+  &:after, &:before{
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  li.tool{
+    display: inline-block;
+    color: #555;
+    font-size: 16px;
+    padding: 8px;
+    vertical-align: top;
+
+    &.right{
+      float: right;
+    }
+
+    .release{
+      display: inline-block;
+      height: 16px;
+      line-height: 16px;
+      vertical-align: 0.05em;
+      font-size: 13px;
+      margin-left: 5px;
+    }
+  }
+`
