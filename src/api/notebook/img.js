a7e1223604781a0f0d71469cf8ae09194ceda6dd
xtthaop <961116034@qq.com>
2024-05-27 23:31:12 +0800

    恢复和彻底删除回收站图片

--- src/api/notebook/img.js
+++ src/api/notebook/img.js
@@ -1,9 +1,27 @@
 import request from '@/utils/request'
 
-// 新增笔记
+// 获取所有被删除的图片
 export function getBackupImgs() {
   return request({
     url: '/restful/note_img/get_backup_imgs',
     method: 'get',
+  })
+}
+
+// 恢复被删除的图片
+export function restoreImg(data) {
+  return request({
+    url: '/restful/note_img/restore_note_img',
+    method: 'put',
+    data,
+  })
+}
+
+// 彻底删除图片
+export function completelyDeleteImg(data) {
+  return request({
+    url: '/restful/note_img/completely_delete_img',
+    method: 'delete',
+    data,
   })
 }
--- src/views/recycle-bin/img.vue
+++ src/views/recycle-bin/img.vue
@@ -7,8 +7,47 @@
         <span>张图片</span>
       </div>
       <div>
-        <div class="img-item" v-for="item in imgList" :key="item">
-          <img :src="`${item.split('.')[0]}_low_ratio.${item.split('.')[1]}`" :data-src="item" />
+        <div
+          class="img-item"
+          :class="{ restored: item.restored }"
+          v-for="(item, index) in imgList"
+          :key="item"
+        >
+          <img
+            :src="`${item.url.split('.')[0]}_low_ratio.${item.url.split('.')[1]}`"
+            :data-src="item.url"
+          />
+
+          <div class="handle-wrapper">
+            <div>
+              <el-button type="primary" @click="handleRestoreImg(item)" :loading="restoreLoading">
+                恢复图片
+              </el-button>
+              <br />
+              <el-button
+                type="danger"
+                @click="handleDeleteImg(item.url, index)"
+                :disabled="restoreLoading"
+                style="margin-top: 10px"
+              >
+                彻底删除
+              </el-button>
+            </div>
+          </div>
+
+          <div class="restored-info">
+            <div>
+              <div class="tip">已恢复</div>
+              <el-button
+                :type="item.copied ? 'primary' : ''"
+                :icon="item.copied ? Select : DocumentCopy"
+                round
+                @click="handleCopy(item)"
+              >
+                <span>{{ item.copied ? '复制成功' : '复制文本' }}</span>
+              </el-button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -17,7 +56,8 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import { getBackupImgs } from '@/api/notebook/img'
+import { getBackupImgs, restoreImg, completelyDeleteImg } from '@/api/notebook/img'
+import { Select, DocumentCopy } from '@element-plus/icons-vue'
 import useImgLazyLoad from '../preview/img-lazy-load'
 
 defineOptions({
@@ -41,6 +81,37 @@ function handleGetBackupImgs() {
   getBackupImgs()
     .then((res) => {
       imgList.value = res.data
+    })
+    .finally(() => {
+      listLoading.value = false
+    })
+}
+
+const restoreLoading = ref(false)
+
+function handleRestoreImg(item) {
+  restoreLoading.value = true
+  restoreImg({ img_path: item.url })
+    .then(() => {
+      item.restored = true
+    })
+    .finally(() => {
+      restoreLoading.value = false
+    })
+}
+
+function handleCopy(item) {
+  const text = `![${item.update_time}.png](${item.url.replace('uploads_clear_backup', 'uploads')})`
+  navigator.clipboard.writeText(text).then(() => {
+    item.copied = true
+  })
+}
+
+function handleDeleteImg(url, index) {
+  listLoading.value = true
+  completelyDeleteImg({ img_path: url })
+    .then(() => {
+      imgList.value.splice(index, 1)
     })
     .finally(() => {
       listLoading.value = false
@@ -75,6 +146,7 @@ function handleGetBackupImgs() {
   }
 
   .img-item {
+    position: relative;
     display: inline-block;
     width: calc(20% - 10px);
     aspect-ratio: 1;
@@ -82,12 +154,44 @@ function handleGetBackupImgs() {
     border: 1px solid #eee;
     border-radius: 5px;
     overflow: hidden;
+    cursor: pointer;
+
+    &:not(.restored):hover .handle-wrapper {
+      display: flex;
+    }
+
+    &.restored .restored-info {
+      display: flex;
+    }
 
     img {
       width: 100%;
       height: 100%;
       object-fit: cover;
       object-position: center;
+    }
+
+    .handle-wrapper,
+    .restored-info {
+      display: none;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(255, 255, 255, 0.5);
+    }
+
+    .restored-info {
+      .tip {
+        margin-bottom: 20px;
+        color: var(--base-primary-color);
+        font-size: 20px;
+        text-align: center;
+      }
     }
   }
 }
